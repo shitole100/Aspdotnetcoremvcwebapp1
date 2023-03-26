f450d4f89b82c12f192de6d8980cf15d1ad25065
Admin <Admin@DESKTOP-0C3M1BG>
2023-03-26 21:57:07 +0530

    Add project files.

---
+++ Models/Mvcapp1Context.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aspdotnetcoremvcwebapp1.Models;
+
+public partial class Mvcapp1Context : DbContext
+{
+    public Mvcapp1Context()
+    {
+    }
+
+    public Mvcapp1Context(DbContextOptions<Mvcapp1Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=MVCAPP1cs");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__USERS__3214EC27657542DD");
+
+            entity.ToTable("USERS");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Gender)
+                .HasMaxLength(10)
+                .IsUnicode(false)
+                .HasColumnName("GENDER");
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("NAME");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
